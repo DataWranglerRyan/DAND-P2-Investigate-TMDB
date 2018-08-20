72762f3768310f42f1aefb8731d5406c2b9e0be4
Ryan Gee <40322212+DataWranglerRyan@users.noreply.github.com>
2018-08-19 23:02:38 -0700

    created career length mean rating plot

--- models/the_movie_database.py
+++ models/the_movie_database.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 class TheMovieDatabase(object):
@@ -7,6 +8,15 @@ class TheMovieDatabase(object):
                           'revenue_adj', 'vote_average', 'vote_count']
         self.df_orig = pd.read_csv(path, index_col=self.req_index)
         self.df = self.__explode_cast()
+        self.chart_colors = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+        # Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.
+        for i in range(len(self.chart_colors)):
+            r, g, b = self.chart_colors[i]
+            self.chart_colors[i] = (r / 255., g / 255., b / 255.)
 
     def __explode_cast(self):
         """
@@ -41,15 +51,25 @@ class TheMovieDatabase(object):
             'release_year': 'min',
             'release_year_dup': 'max'
         }
-        # ['release_year', 'actor']
         self.df['release_year_dup'] = self.df['release_year']
+        # ['release_year', 'actor']
         actor_stats_df = self.df.groupby(['actor'], as_index=False)\
             .agg(agg_functions).rename(columns=rename_col).sort_values('number_of_movies', ascending=False)
-        # Create actor career length column
-        actor_stats_df['career_length'] = actor_stats_df['last_year'] - actor_stats_df['first_year']
-        # Create bins for actor career length
-        bin_edges = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 100]
-        bin_names = ['0-5', '5-10', '10-15', '15-20', '20-25', '25-30', '30-35', '35-40', '40-45', '45-50', '50+']
-        actor_stats_df['career_length_bin'] = pd.cut(actor_stats_df['career_length'], bin_edges, labels=bin_names)
         return actor_stats_df
+
+    def get_career_len_mean(self):
+        df = self.get_actor_metrics()
+        # Create actor career length column
+        df['career_length'] = df['last_year'] - df['first_year']
+        # Create bins for actor career length
+        bin_edges = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 100]
+        bin_names = ['0-5', '5-10', '10-15', '15-20', '20-25', '25-30', '30-35', '35-40', '40-45', '45+']
+        df['career_length_bin'] = pd.cut(df['career_length'], bin_edges, labels=bin_names)
+        return df.groupby('career_length_bin', as_index=False)['vote_average'].mean()
+
+    def plot_career_len_mean(self):
+        self.get_career_len_mean().plot.bar(x='career_length_bin', y='vote_average', ylim=5, color=self.chart_colors[0])
+        plt.savefig("../figures/foo.pdf", bbox_inches='tight')
+        plt.show()
+
 
--- script/run.py
+++ script/run.py
@@ -1,18 +1,14 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 from models.the_movie_database import TheMovieDatabase
 
 tmdb = TheMovieDatabase('../data/tmdb-movies.csv')
-actor_df = tmdb.get_actor_metrics()
-print(actor_df)
-actor_df.plot.scatter(x='career_length', y='vote_average')
-plt.show()
-
-# locations = [1, 2, 3, 4]
-# height = [10, 20, 30, 40]
-# labels = ['High', 'Moderately High', 'Medium', 'Low']
-# f = plt.figure()
-# plt.plot(locations, height, 'go-', label='line 1')
+# actor_df = tmdb.get_actor_metrics()
+# print(actor_df)
+# actor_df.plot.scatter(x='career_length', y='vote_average')
 # plt.show()
-# f.savefig("../figures/foo.pdf", bbox_inches='tight')
 
+career_df = tmdb.get_career_len_mean()
+tmdb.plot_career_len_mean()
+print(career_df)
 print('done')
