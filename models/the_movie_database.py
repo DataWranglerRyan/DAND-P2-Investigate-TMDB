6909f272b39967ec2e5afd6e65555bd8f3e75486
Ryan Gee <40322212+DataWranglerRyan@users.noreply.github.com>
2018-08-26 23:37:05 -0700

    added analysis report and jupyter notebook

--- models/the_movie_database.py
+++ models/the_movie_database.py
@@ -7,7 +7,7 @@ class TheMovieDatabase(object):
         self.req_index = ['id', 'original_title', 'popularity', 'release_year',
                           'revenue_adj', 'vote_average', 'vote_count']
         self.df_orig = pd.read_csv(path, index_col=self.req_index)
-        self.df = self.__explode_cast()
+        self.df_exploded = self.__explode_cast()
         self.chart_colors = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
              (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
              (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
@@ -51,12 +51,12 @@ class TheMovieDatabase(object):
             'release_year': 'min',
             'release_year_dup': 'max'
         }
-        self.df['release_year_dup'] = self.df['release_year']
-        actor_stats_df = self.df.groupby(['actor'], as_index=False)\
+        self.df_exploded['release_year_dup'] = self.df_exploded['release_year']
+        actor_stats_df = self.df_exploded.groupby(['actor'], as_index=False)\
             .agg(agg_functions).rename(columns=rename_col).sort_values('number_of_movies', ascending=False)
         return actor_stats_df
 
-    def get_avg_rating_by_career_len(self):
+    def get_data_grouped_by_career_len(self):
         df = self.get_actor_metrics()
         # Create actor career length column
         df['career_length'] = df['last_year'] - df['first_year']
@@ -64,7 +64,10 @@ class TheMovieDatabase(object):
         bin_edges = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 100]
         bin_names = ['0-5', '5-10', '10-15', '15-20', '20-25', '25-30', '30-35', '35-40', '40-45', '45+']
         df['career_length_bin'] = pd.cut(df['career_length'], bin_edges, labels=bin_names)
-        return df.groupby('career_length_bin', as_index=False)['vote_average'].mean()
+        return df.groupby('career_length_bin', as_index=False)
+
+    def get_avg_rating_by_career_len(self):
+        return self.get_data_grouped_by_career_len()['vote_average'].mean()
 
     def plot_avg_rating_by_career_len(self, save=False):
         plt.style.use(self.chart_style)
@@ -78,6 +81,23 @@ class TheMovieDatabase(object):
         plt.ylabel('Average Movie Rating', fontsize=10, alpha=0.7)
         if save:
             plt.savefig("../figures/avg_rating_by_career_len.pdf", bbox_inches='tight')
+        plt.show()
+
+    def get_num_actors_by_career_len(self):
+        return self.get_data_grouped_by_career_len().agg({'actor': pd.Series.nunique})
+
+    def plot_num_actors_by_career_len(self, save=False):
+        plt.style.use(self.chart_style)
+        self.get_num_actors_by_career_len().plot.bar(x='career_length_bin', y='actor', ylim=5,
+                                            color=self.chart_colors[0], legend=False)
+        ax = plt.subplot(111)
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        plt.title('How many actors/actresses are in each career length bin?', fontsize=14)
+        plt.xlabel('Career Length (years)', fontsize=10, alpha=0.7)
+        plt.ylabel('Number of Actors', fontsize=10, alpha=0.7)
+        if save:
+            plt.savefig("../figures/num_actors_by_career_len.pdf", bbox_inches='tight')
         plt.show()
 
     def get_avg_rating_by_movie_exp(self):
@@ -114,7 +134,7 @@ class TheMovieDatabase(object):
             'vote_count': 'sum',
             'revenue_adj': 'mean'
         }
-        actor_stats_df = self.df.groupby(['release_year', 'actor'], as_index=False)\
+        actor_stats_df = self.df_exploded.groupby(['release_year', 'actor'], as_index=False)\
             .agg(agg_functions).rename(columns=rename_col).sort_values('release_year', ascending=False)
         actor_stats_df = actor_stats_df[actor_stats_df.actor == actor_name]
         if not actor_stats_df.shape[0]:
--- script/run.py
+++ script/run.py
@@ -1,6 +1,6 @@
 from models.the_movie_database import TheMovieDatabase
 
-save = True
+save = False
 tmdb = TheMovieDatabase('../data/tmdb-movies.csv')
 tmdb.plot_avg_rating_by_career_len(save=save)
 tmdb.plot_avg_rating_by_movie_exp(save=save)
